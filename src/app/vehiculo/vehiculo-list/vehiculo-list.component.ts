0033afb800594b3e5c57f10db973cdc9589f917c
CaviedesGitHub <l.padillac2@uniandes.edu.co>
2022-04-29 18:17:43 -0500

    Punto2: Modificación Listado de Autos Usados.

--- src/app/vehiculo/vehiculo-list/vehiculo-list.component.ts
+++ src/app/vehiculo/vehiculo-list/vehiculo-list.component.ts
@@ -9,16 +9,35 @@ import { VehiculoService } from '../vehiculo.service';
 })
 export class VehiculoListComponent implements OnInit {
   vehiculos: Array<Vehiculo>=[];
+  mapaTotal:Map<string,number>=new Map();
   constructor(private vehiculoService: VehiculoService) { }
 
   getVehiculos(): void {
     this.vehiculoService.getVehiculos().subscribe((vehiculos)=>{
       this.vehiculos=vehiculos;
+      this.totalizaDatos();
     })
   }
 
   ngOnInit() {
     this.getVehiculos();
+  }
+
+  totalizaDatos():void{
+    this.mapaTotal.clear();
+    this.vehiculos.forEach((veh)=>{
+      if (this.mapaTotal.has(veh.marca)){
+        let conteo=this.mapaTotal.get(veh.marca)!;
+        this.mapaTotal.set(veh.marca, conteo+1);
+      }
+      else{
+        this.mapaTotal.set(veh.marca, 1);
+      }
+    });
+  };
+
+  asIsOrder(a:any, b:any){
+    return 1;
   }
 
 }
