8c378ee8a35b20dc5bfd6b3fe34c2fda7cb27c2c
CaviedesGitHub <l.padillac2@uniandes.edu.co>
2022-04-29 21:03:11 -0500

    Punto3: Prueba del Componente Vehiculo-list.

--- src/app/vehiculo/vehiculo-list/vehiculo-list.component.spec.ts
+++ src/app/vehiculo/vehiculo-list/vehiculo-list.component.spec.ts
@@ -4,14 +4,22 @@ import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
 import { VehiculoListComponent } from './vehiculo-list.component';
+import { HttpClientModule } from '@angular/common/http';
+
+import { Vehiculo } from '../vehiculo';
+import { VehiculoService} from '../vehiculo.service';
+
 
 describe('VehiculoListComponent', () => {
   let component: VehiculoListComponent;
   let fixture: ComponentFixture<VehiculoListComponent>;
+  let debug: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ VehiculoListComponent ]
+      imports: [HttpClientModule],
+      declarations: [ VehiculoListComponent ],
+      providers: [ VehiculoService]
     })
     .compileComponents();
   }));
@@ -19,10 +27,24 @@ describe('VehiculoListComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(VehiculoListComponent);
     component = fixture.componentInstance;
+
+    component.vehiculos = [
+      new Vehiculo(1, "Chevrolet", "Onix", "LT", 2022, 4839, "Gris Satin", ""),
+      new Vehiculo(2, "Renault", "Kangoo", "VU Express", 2017, 93272, "Blanco", ""),
+      new Vehiculo(3, "Chevrolet", "Spark", "Life", 2018, 55926, "Plata", "")
+    ];
+
     fixture.detectChanges();
+    debug = fixture.debugElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('Deberia Tener Tabla con 3 filas y un encabezado.', () => {
+    expect(fixture.debugElement.queryAll(By.css('tbody > tr')).length).toEqual(component.vehiculos.length);
+    expect(fixture.debugElement.queryAll(By.css('thead > tr')).length).toEqual(1);
+  });
+
 });
